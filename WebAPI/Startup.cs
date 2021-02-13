9688a3f94558cdaf8eb6b2cbbc0cc598d2c3588b
Yusuffkoc <48139038+Yusuffkoc@users.noreply.github.com>
2021-02-13 23:55:21 +0300

    API

---
+++ WebAPI/Startup.cs
@@ -0,0 +1,62 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Business.Abstract;
+using Business.Concrete;
+using DataAccess.Abstract;
+using DataAccess.Concrete.EntityFramework;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace WebAPI
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            //AUTOFAC BÝZE AOP sunuyor
+            //Autofac,Ninject,CastleWindsor,StructureMap,LightInject,DryInject  --> IoC Container 
+            
+            services.AddControllers();
+            //IProductservice baglýlýk gorursen o product manager dir diyor.bizim yerimize newliyor.(içinde data yoksa yap)
+            services.AddSingleton<IProductService, ProductManager>();
+            //Iproductservicemiz Iproducdala baglý  öyleyse efproductdal üret.
+            services.AddSingleton<IProductDal, EfProductDal>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
